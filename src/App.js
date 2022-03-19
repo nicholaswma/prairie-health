144ba7122704fa857750df78ec90cdb2cf412487
Nicholas Ma <nicholasma@nicholass-mbp.lan>
2022-03-19 10:45:05 -0700

    adding media queries for responsive design

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import dummy from "./Data/dummy.json";
 import Header from "./Components/Header.js";
 import PatientSelectForm from "./Components/PatientSelectForm.js";
 import MyResponsiveChart from "./Components/MyResponsiveChart.js";
-import { Component } from "react";
-import { ResponsiveLine } from "@nivo/line";
+import AnalyzerLogo from "./Assets/Analyzer.png";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function App() {
   const [selected, setSelected] = useState("");
   const [displayData, setDisplayData] = useState([]);
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
   useEffect(() => {
     const sortDisplayData = () => {
@@ -22,9 +26,11 @@ function App() {
         return individual["Patient Name"] === selected;
       });
       individualData.forEach((ele) => {
+        let timeStamp = ele["Timestamp"].split(" ")[0];
         gad7.push({ x: ele["Timestamp"], y: ele["GAD-7 Score"] });
       });
       individualData.forEach((ele) => {
+        let timeStamp = ele["Timestamp"].split(" ")[0];
         gad10.push({ x: ele["Timestamp"], y: ele["PHQ-9 Score"] });
       });
       returnData = [
@@ -37,16 +43,54 @@ function App() {
   }, [selected]);
 
   return (
-    <Box>
-      {/* <Header /> */}
-      <Typography fontSize={"2rem"} textAlign={"center"} fontWeight={800}>
-        Prairie Health
-        {console.log("here", setDisplayData)}
-      </Typography>
-      <Box padding={"2em"}>
-        <PatientSelectForm selected={selected} setSelected={setSelected} />
+    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#DBCCBA",
+        }}
+        height="100vh"
+      >
+        <img src={AnalyzerLogo} alt="logo" height="25%" />
+        <Box padding={"2em"}>
+          <PatientSelectForm selected={selected} setSelected={setSelected} />
+        </Box>
+        <Box padding={"2em"} alignContent="center" justifyContent="center">
+          <SettingsIcon onClick={() => console.log("open modal")} />
+        </Box>
       </Box>
-      <MyResponsiveChart data={displayData} />
+      {selected ? (
+        <Box width="100%" height="100vh">
+          <Typography
+            fontSize={"2rem"}
+            fontWeight={800}
+            paddingBottom="0"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {selected}
+          </Typography>
+          <MyResponsiveChart data={displayData} />
+        </Box>
+      ) : (
+        <Box
+          width="100%"
+          height="100vh"
+          sx={{
+            bgcolor: "#D3D3D3",
+            opacity: 0.5,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography>Please Select a Patient</Typography>
+        </Box>
+      )}
     </Box>
   );
 }
